e5dc6305b44d26702b0d8b5ac2cdfa2ba461613b
genxium <genxium@hotmail.com>
2020-07-01 09:55:41 +0800

    Added "p164_Maximum_Gap".

---
+++ p164_Maximum_Gap/submission.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    /*
+    RadixSort codes copied from geeksforgeeks.
+    */
+    int getMax(int arr[], int n) { 
+        int mx = arr[0]; 
+        for (int i = 1; i < n; i++) 
+            if (arr[i] > mx) 
+                mx = arr[i]; 
+        return mx; 
+    } 
+  
+    void countSort(int arr[], int n, int exp) { 
+        int output[n];
+        int i, count[10] = {0}; 
+  
+        for (i = 0; i < n; i++) 
+            count[ (arr[i]/exp)%10 ]++; 
+  
+        for (i = 1; i < 10; i++) 
+            count[i] += count[i - 1]; 
+  
+        for (i = n - 1; i >= 0; i--) { 
+            output[count[ (arr[i]/exp)%10 ] - 1] = arr[i]; 
+            count[ (arr[i]/exp)%10 ]--; 
+        } 
+  
+        for (i = 0; i < n; i++) 
+            arr[i] = output[i]; 
+    } 
+  
+    void radixsort(int arr[], int n) { 
+        int m = getMax(arr, n); 
+        for (int exp = 1; m/exp > 0; exp *= 10) 
+            countSort(arr, n, exp); 
+    } 
+    
+    int maximumGap(vector<int>& nums) {
+        int n = nums.size();
+        if (2 > n) return 0;
+        radixsort(&nums[0], nums.size());
+        int gap = INT_MIN;
+        for (int i = 0; i < n-1; ++i) {
+            int cand = abs(nums[i+1] - nums[i]);
+            if (cand > gap) gap = cand;
+        }
+        return gap;
+    }
+};
