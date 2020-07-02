6285af1c702f1fc76ad0780a06c89ed4eb203f5d
genxium <genxium@hotmail.com>
2020-07-02 13:14:21 +0800

    Updated "Concurrency/p1115_Print_FooBar_Alternately".

--- Concurrency/p1115_Print_FooBar_Alternately/submission_pthread_mutex_dirty.cpp
+++ Concurrency/p1115_Print_FooBar_Alternately/submission_pthread_mutex_dirty.cpp
@@ -3,8 +3,15 @@ private:
     int n;
     pthread_mutex_t fooMux = PTHREAD_MUTEX_INITIALIZER;
     pthread_mutex_t barMux = PTHREAD_MUTEX_INITIALIZER;
-    pthread_cond_t fooCond = PTHREAD_COND_INITIALIZER;
-    pthread_cond_t barCond = PTHREAD_COND_INITIALIZER;
+    
+    /*
+    This solution is dirty because it uses "thread#b to unlock a mutex held by thread#a", which generally results in "undefined behaviour" for most "pthread_mutex_t types".
+    
+    Moreover, if we use "pthread_mutex_t type `PTHREAD_MUTEX_ERRORCHECK`", this dirty solution will fail.
+    
+    // pthread_mutex_t fooMux = PTHREAD_ERRORCHECK_MUTEX_INITIALIZER_NP;
+    // pthread_mutex_t barMux = PTHREAD_ERRORCHECK_MUTEX_INITIALIZER_NP;
+    */
 
 public:
     FooBar(int n) {
@@ -18,7 +25,7 @@ public:
         for (int i = 0; i < n; i++) {
             pthread_mutex_lock(&fooMux);
         	// printFoo() outputs "foo". Do not change or remove this line.
-        	printFoo();
+            printFoo();
             pthread_mutex_unlock(&barMux); // The problem description says that "the same instance of `FooBar` will be passed to two different threads".
         }
     }
@@ -27,7 +34,7 @@ public:
         for (int i = 0; i < n; i++) {
             pthread_mutex_lock(&barMux);
         	// printBar() outputs "bar". Do not change or remove this line.
-        	printBar();
+            printBar();
             pthread_mutex_unlock(&fooMux);
         }
     }
