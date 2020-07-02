c82ee2a001c4dfb3fdca8f8613023d1304033db7
genxium <genxium@hotmail.com>
2020-07-02 14:06:52 +0800

    Added "Concurrency/p1115_Print_FooBar_Alternately/submission_pthread_mutex_single_mux_TLE".

---
+++ Concurrency/p1115_Print_FooBar_Alternately/submission_pthread_mutex_single_mux_TLE.cpp
@@ -0,0 +1,44 @@
+class FooBar {
+private:
+    int n, counter, counterUpper;
+    pthread_mutex_t sharedMux = PTHREAD_MUTEX_INITIALIZER;
+    
+public:
+    FooBar(int n) {
+        this->n = n;
+        this->counter = 0;
+        this->counterUpper = (n << 1);
+    }
+
+    void foo(function<void()> printFoo) {
+        while (true) {
+            pthread_mutex_lock(&sharedMux);
+            if (counter >= counterUpper) {
+                pthread_mutex_unlock(&sharedMux);
+                break;
+            }
+            if ((counter&1) == 0) {
+                //printf("counter == %d, foo\n", counter);
+                printFoo();
+                ++counter;
+            } 
+            pthread_mutex_unlock(&sharedMux);
+        }
+    }
+
+    void bar(function<void()> printBar) {
+        while (true) {
+            pthread_mutex_lock(&sharedMux);
+            if (counter >= counterUpper) {
+                pthread_mutex_unlock(&sharedMux);
+                break;
+            }
+            if ((counter&1) == 1) {
+                //printf("counter == %d, bar\n", counter);
+                printBar();
+                ++counter;
+            } 
+            pthread_mutex_unlock(&sharedMux);
+        }
+    }
+};
