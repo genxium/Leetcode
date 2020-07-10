001f6122048fc6e390b7ea820d654b26c6c95502
genxium <genxium@hotmail.com>
2020-07-10 08:37:25 +0800

    Added "Concurrency/cas_practice.cpp".

---
+++ Concurrency/cas_practice.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <atomic>
+using namespace std;
+
+atomic<int> counter;
+
+void blocksOnCounter(int const expectedOld) {
+  // This function only serves to spinlock the current KernelThread w.r.t. certain value of "counter", it's NOT TRUE that the whole function being atomic.
+  int localContainer = expectedOld;
+  while (true) {
+    // counter.compare_exchange_weak(localContainer, expectedOld+1);
+    counter.compare_exchange_strong(localContainer, expectedOld+1);
+    int afterCompareExchangeCounterVal = counter.load();
+    printf("afterCompareExchangeCounterVal == %d\n", afterCompareExchangeCounterVal);
+    if (localContainer == expectedOld) break;
+    localContainer = expectedOld;
+  }
+}
+
+int main() {
+  /*
+  g++ --std=c++11 -o cas_practice cas_practice.cpp
+  */
+  counter = 0;
+  //int expectedOld = 0; // won't block and will only print "afterCompareExchangeCounterVal == 1" once
+  int expectedOld = 99; // will block and keep printing "afterCompareExchangeCounterVal == 0"
+  blocksOnCounter(expectedOld);
+  return 0;
+}
