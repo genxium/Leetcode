73f15b00db4880be6d6b08d782f442000acb277f
genxium <genxium@hotmail.com>
2020-07-20 12:43:30 +0800

    Minor update.

--- p260_Single_Number_III/submission.cpp
+++ p260_Single_Number_III/submission.cpp
@@ -11,17 +11,23 @@ public:
       
       // now that xorRes contains (a^b)
       
-      // find the "LowestSignificantBit" of "xorRes"
-      int pow2 = 1;
+      // find the "LowestSignificantBit" of "xorRes", it's actually OK to find any bit "1" in "xorRes", and move on to the following steps
+      int chosenShift = 1;
       for (int lsb = 0; lsb < 32; ++lsb) {
-        if ((xorRes & pow2) > 0) break;
-        pow2 <<= 1;
+        if ((xorRes & chosenShift) > 0) break;
+        chosenShift <<= 1;
       }
+
+      /* 
+      now that all elements in "nums[]" are deemed one type or another
+      - those having the same chosen bit "1" as "xorRes", i.e. "chosenShift"
+      - those not having the same chosen bit "1" as "xorRes"
+      */
       
       // get "a" and "b"
       int a = 0, b = 0;
       for (int i = 0; i < n; ++i) {
-        if ((nums[i] & pow2) > 0) {
+        if ((nums[i] & chosenShift) > 0) {
           a ^= nums[i];
         } else {
           b ^= nums[i];
